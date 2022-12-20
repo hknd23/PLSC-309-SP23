a2ad069ec861cfcbd28e42c4cbf2fda2d6f302a1
Nguyen Huynh <35204932+hknd23@users.noreply.github.com>
2022-12-20 14:15:45 -0500

    Create RBasics.R

---
+++ Sample-code/RBasics.R
@@ -0,0 +1,53 @@
+#: Use this to make comments and annotations. 
+# This is a script, the file that contains the code input, which R will use to run through. 
+
+#Basic math operations: 
+
+1+1 
+2*2 
+3/4
+6^9 
+
+#A function is a piece of code that does a specific task 
+
+#calculating a square root
+sqrt(49) 
+
+#Print out a sentence
+print("Hello World")
+
+#Data can take the following types: 
+
+#Numeric: Real numbers
+
+1.5
+2
+-4.9999
+1/5
+
+#Integers: Whole numbers
+
+3
+4
+0
+
+#String: Characters, letters, words, sentences. Must be wrapped in " " or ' ' 
+
+"Hello!" 
+"Political Science" 
+"4"
+"A number wrapped in quotations is considered a string" 
+
+#Logical (Boolean): TRUE or FALSE
+
+TRUE
+FALSE
+
+#Data are stored in object. You can name an object and then call it. R uses <- to create and assign values to objects
+
+#Single value
+
+Bread<-26
+print(Bread)
+
+
